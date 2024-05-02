a6e1bd079904a1cfa33390f422199927d1cd4867
Lewis Ubebe <140450161+lubebe23@users.noreply.github.com>
2024-05-02 14:10:25 +0100

    TemperatureConverter.cs
     *               This program asks the user to choose a temperature conversion
     *              calculator, either Celcius to Fahrenheit or Fahrenheit to Celcius,
     *              then asks the user to enter a whole number to convert. The calculator
     *              performs the conversion using the appropriate formula and outputs
     *              the result to the console as a decimal rounded to one place. 
     *              Any invalid inputs result in an error message including entering a decimal 
     *              and a non number error too.
     *              Error catching nis also present for invalid menu entry.
     *              
    */

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿using System;
 
+* Name:        temperatureConversion
+ * 
+ * Author:      Lewis Ubebe
+ * 
+ * Version:     1 — This program asks the user to choose a temperature conversion
+ *              calculator, either Celcius to Fahrenheit or Fahrenheit to Celcius,
+ *              then asks the user to enter a whole number to convert. The calculator
+ *              performs the conversion using the appropriate formula and outputs
+ *              the result to the console as a decimal rounded to one place. 
+ *              Any invalid inputs result in an error message including entering a decimal 
+ *              and a non number error too.
+ *              Error catching nis also present for invalid menu entry.
+ *              
+*/
+     
 namespace TemperatureConversion
 {
     class Program
